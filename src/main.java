c8e633f469e3371da489528aa90963b1dc1d60b8
Michal <michal.kornak2@gmail.com>
2021-11-08 15:09:02 +0100

    skończone

--- src/main.java
+++ src/main.java
@@ -24,9 +24,20 @@ public class main {
                         tab[k][n] = tab[k-1][n-1] + tab[k][n-1];
                     }
 
-
+        for (int i=0; i<=K; i++)
+        {
+            for (int j=0; j<=N; j++)
+            {
+                int val = tab[i][j];
+                if (val<100) System.out.print(" ");
+                if (val<10) System.out.print(" ");
+                System.out.print(val+" ");
+            }
+            System.out.println();
+        }
+    }
 
     }
 
-}
+
 
